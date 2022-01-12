e10e56ec09afe53a5126299a448e9a6572b62f10
muirisc <91299876+muirisc@users.noreply.github.com>
2022-01-11 23:18:16 -0500

    commit tree 81aa4f209b8bc015c5bccfc2e69d40181ff40d6d

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250}
+  validates :summary, length: { maximum: 250}
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+
+  validate :click_bait?
+
+  VIABLE_TITLES = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+  
+  def click_bait?
+  if VIABLE_TITLES.none? {|pat| pat.match title}
+      errors.add(:title, "Not clickbait-y enough")
+    end
+  end
 end
